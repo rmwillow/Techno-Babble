a8613ddd5a496d1018ca910777fb607785a1c9fd
rmwillow <52016382+rmwillow@users.noreply.github.com>
2021-08-02 11:48:47 -0500

    connecting server

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,7 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api/');
-const homeRoutes = require('./api/home-routes.js');
+const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
 
 router.use('/', homeRoutes);
-router.use('/dashboard', dashboardRoutes);
-router.use('/api', apiRoutes);
-
-module.exports = router;
