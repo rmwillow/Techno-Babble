c31e1be02248f587bdc035517a31b0fd004ddbf0
rmwillow <52016382+rmwillow@users.noreply.github.com>
2021-08-03 18:02:39 -0500

    renamed schemea

--- config/config.js
+++ config/config.js
@@ -3,6 +3,7 @@ const Sequelize = require('sequelize');
 //allow for use of .env data
 require('dotenv').config();
 
+
 // create connection to db, whether local on on heroku using jawsdb
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
