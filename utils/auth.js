0d13cc80169f24ca4ef48e497fe6db57d452867a
rmwillow <52016382+rmwillow@users.noreply.github.com>
2021-08-03 17:21:55 -0500

    added update connection and post

--- utils/auth.js
+++ utils/auth.js
@@ -1,6 +1,8 @@
+// middleware to verify user loggied in before restricted route access given
 const withAuth = (req, res, next) => {
-    if (!req.session.userId) {
-      res.redirect("/login");
+  if (!req.session.userId) {
+    res.redirect("/login");
+
     } else {
       next();
     }
